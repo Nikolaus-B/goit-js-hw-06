1192ebe59bfbf63fdb866a734459b6b7736c4a5d
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2023-08-22 17:15:12 +0300

    finish3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i++) {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredients[i];
+  elements.push(li);
+}
+
+list.append(...elements);
